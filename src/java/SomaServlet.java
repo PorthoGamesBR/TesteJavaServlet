0e214d74403f8883b113b94c9b6ee4adb3b150c2
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2022-12-21 01:06:53 -0300

    Revert "Revert "Mudança no HTML doGet do servlet""
    This reverts commit ed6cbc3b89ff3df4abec64dbe70b1ba0b2172aec.

--- src/java/SomaServlet.java
+++ src/java/SomaServlet.java
@@ -32,7 +32,9 @@ public class SomaServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        int p1 = Integer.parseInt(request.getParameter("p1"));
+        int p2 = Integer.parseInt(request.getParameter("p2"));
+        int total = p1 + p2;
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -42,7 +44,7 @@ public class SomaServlet extends HttpServlet {
             out.println("<title>Resultado da soma</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet SomaServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>O resultado foi: " + total + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
